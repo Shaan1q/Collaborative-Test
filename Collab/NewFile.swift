3fbaeb1231cec9659ae466ca19f958b2e8f409a5
competitioner <anon>
2026-04-23 13:56:14 -0400

    Added a new File

---
+++ Collab/NewFile.swift
@@ -0,0 +1,18 @@
+//
+//  NewFile.swift
+//  Collab
+//
+//  Created by Shaanzay Iqbal on 4/23/26.
+//
+
+import SwiftUI
+
+struct NewFile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewFile()
+}
