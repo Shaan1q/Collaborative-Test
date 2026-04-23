a32a757fec7acd08a292b7ecc687b6b311f97cb0
competitioner <anon>
2026-04-23 13:45:30 -0400

    Initial Commit

---
+++ Collab/CollabApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollabApp.swift
+//  Collab
+//
+//  Created by Shaanzay Iqbal on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct CollabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
