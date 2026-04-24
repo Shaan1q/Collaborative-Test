7e918ed9706672d0a3cdc1a14720d2653316c335
BT_C09_24 <mobileappdev@bths.edu>
2026-04-24 13:33:34 -0400

    red

--- Collab/ContentView.swift
+++ Collab/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Text("Collab")
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
+                .foregroundStyle(.red)
             Text("Hello, world! HI YES EDITTED")
             Text("This is an added line of code")
             Text ("new line")
